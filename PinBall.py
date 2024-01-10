dde879e9daa41306a816669d8203e73dca51124a
PiriPiri11 <153788607+6610110724@users.noreply.github.com>
2024-01-10 11:17:18 +0700

    Add files via upload

--- PinBall.py
+++ PinBall.py
@@ -1,78 +1,110 @@
-import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Ellipse, Color
 from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
 from random import randint
 
-kivy.require('1.11.1')
+class StartScreen(BoxLayout):
+    def __init__(self, start_game_callback, **kwargs):
+        super(StartScreen, self).__init__(**kwargs)
 
+        self.orientation = 'vertical'
 
-class Ball(Widget):
-    def __init__(self, **kwargs):
-        super(Ball, self).__init__(**kwargs)
-        self.size = (50, 50)
-        self.source = 'ball.png'  # Add an image for the ball
-        self.velocity = (4, 0)  # Initial velocity
+        title_label = Label(text='Pinball Game', font_size='30sp', size_hint=(1, 0.5))
+        start_button = Button(text='Start Game', size_hint=(1, 0.5))
+        start_button.bind(on_press=start_game_callback)
 
-    def move(self):
-        self.pos = (self.pos[0] + self.velocity[0], self.pos[1] + self.velocity[1])
+        self.add_widget(title_label)
+        self.add_widget(start_button)
 
-        # Bounce off walls
-        if (self.x < 0) or (self.right > self.parent.width):
-            self.velocity = (-self.velocity[0], self.velocity[1])
+class GameOverScreen(BoxLayout):
+    def __init__(self, restart_game_callback, **kwargs):
+        super(GameOverScreen, self).__init__(**kwargs)
 
-        # Bounce off ceiling and floor
-        if (self.y < 0) or (self.top > self.parent.height):
-            self.velocity = (self.velocity[0], -self.velocity[1])
+        self.orientation = 'vertical'
 
+        title_label = Label(text='Game Over', font_size='30sp', size_hint=(1, 0.5))
+        restart_button = Button(text='Restart', size_hint=(1, 0.5))
+        restart_button.bind(on_press=restart_game_callback)
 
-class Coin(Widget):
-    def __init__(self, **kwargs):
-        super(Coin, self).__init__(**kwargs)
-        self.size = (40, 40)
-        self.source = 'coin.png'  # Add an image for the coin
-        self.pos = (randint(0, 400), randint(0, 400))  # Random initial position
-
+        self.add_widget(title_label)
+        self.add_widget(restart_button)
 
 class PinballGame(Widget):
-    def __init__(self, **kwargs):
+    def __init__(self, start_screen_callback, game_over_callback, **kwargs):
         super(PinballGame, self).__init__(**kwargs)
-        self.ball = Ball()
-        self.coins = [Coin() for _ in range(5)]  # Create 5 coins
+        self.ball_radius = 20
+        self.ball_pos = [100, 100]  # Initial ball position
+        self.ball_velocity = [5, 5]  # Initial ball velocity
 
-        for coin in self.coins:
-            self.add_widget(coin)
+        self.start_screen_callback = start_screen_callback
+        self.game_over_callback = game_over_callback
 
-        self.add_widget(self.ball)
-
-        Clock.schedule_interval(self.update, 1 / 60.)
+        with self.canvas:
+            Color(1, 1, 1)
+            self.ball = Ellipse(pos=(self.ball_pos[0] - self.ball_radius, self.ball_pos[1] - self.ball_radius),
+                                size=(self.ball_radius * 2, self.ball_radius * 2))
 
     def update(self, dt):
-        self.ball.move()
+        self.move_ball()
+        self.check_collision()
 
-        # Check collisions with coins
-        coins_to_remove = []
-        for coin in self.coins:
-            if self.ball.collide_widget(coin):
-                coins_to_remove.append(coin)
+    def move_ball(self):
+        self.ball_pos[0] += self.ball_velocity[0]
+        self.ball_pos[1] += self.ball_velocity[1]
 
-        for coin in coins_to_remove:
-            self.coins.remove(coin)
-            self.remove_widget(coin)
+        if self.ball_pos[0] - self.ball_radius < 0 or self.ball_pos[0] + self.ball_radius > self.width:
+            self.ball_velocity[0] *= -1
+        if self.ball_pos[1] - self.ball_radius < 0 or self.ball_pos[1] + self.ball_radius > self.height:
+            self.ball_velocity[1] *= -1
 
-        # Check if all coins are collected
-        if not self.coins:
-            self.add_widget(Label(text="You Win!", font_size=40))
-            Clock.unschedule(self.update)  # Stop the game
+        self.ball.pos = (self.ball_pos[0] - self.ball_radius, self.ball_pos[1] - self.ball_radius)
 
+    def check_collision(self):
+        # Add collision detection logic here
+        pass
+
+    def on_touch_down(self, touch):
+        # Add any touch events you want to handle here
+        pass
 
 class PinballApp(App):
     def build(self):
-        return PinballGame()
+        layout = FloatLayout()
 
+        self.start_screen = StartScreen(start_game_callback=self.start_game)
+        layout.add_widget(self.start_screen)
+
+        return layout
+
+    def start_game(self, instance):
+        layout = self.root
+        layout.remove_widget(self.start_screen)
+
+        game = PinballGame(start_screen_callback=self.show_start_screen, game_over_callback=self.show_game_over)
+        layout.add_widget(game)
+
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+
+    def show_start_screen(self):
+        layout = self.root
+        game = layout.children[-1]
+        layout.remove_widget(game)
+
+        self.start_screen = StartScreen(start_game_callback=self.start_game)
+        layout.add_widget(self.start_screen)
+
+    def show_game_over(self):
+        layout = self.root
+        game = layout.children[-1]
+        layout.remove_widget(game)
+
+        game_over_screen = GameOverScreen(restart_game_callback=self.start_game)
+        layout.add_widget(game_over_screen)
 
 if __name__ == '__main__':
     PinballApp().run()
