0425c81e64d9ae83260db64bfcbcdc4920b09201
PiriPiri11 <153788607+6610110724@users.noreply.github.com>
2024-01-11 08:45:36 +0700

    Add files via upload

--- PinBall.py
+++ PinBall.py
@@ -6,7 +6,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Ellipse, Color
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
-from random import randint
+
 
 class StartScreen(BoxLayout):
     def __init__(self, start_game_callback, **kwargs):
@@ -14,40 +14,35 @@ class StartScreen(BoxLayout):
 
         self.orientation = 'vertical'
 
-        title_label = Label(text='Pinball Game', font_size='50sp', size_hint=(1, 0.5))
-        start_button = Button(text='Start Game', size_hint=(1, 0.5))
+        title_label = Label(text='Pinball Game', font_size='75sp', size_hint=(1, 0.5))
+        start_button = Button(text='Start Game', font_size='35sp', size_hint=(1, 0.5) ,color='yellow')
         start_button.bind(on_press=start_game_callback)
 
         self.add_widget(title_label)
         self.add_widget(start_button)
-
-class GameOverScreen(BoxLayout):
-    def __init__(self, restart_game_callback, **kwargs):
-        super(GameOverScreen, self).__init__(**kwargs)
-
-        self.orientation = 'vertical'
-
-        title_label = Label(text='Game Over', font_size='30sp', size_hint=(1, 0.5))
-        restart_button = Button(text='Restart', size_hint=(1, 0.5))
-        restart_button.bind(on_press=restart_game_callback)
-
-        self.add_widget(title_label)
-        self.add_widget(restart_button)
+        
 
 class PinballGame(Widget):
-    def __init__(self, start_screen_callback, game_over_callback, **kwargs):
+    def __init__(self, start_screen_callback, **kwargs):
         super(PinballGame, self).__init__(**kwargs)
         self.ball_radius = 20
-        self.ball_pos = [100, 100]  # Initial ball position
-        self.ball_velocity = [5, 5]  # Initial ball velocity
-
+        self.ball_pos = [100, 100]
+        self.ball_velocity = [5, 5]
         self.start_screen_callback = start_screen_callback
-        self.game_over_callback = game_over_callback
 
         with self.canvas:
             Color(1, 1, 1)
             self.ball = Ellipse(pos=(self.ball_pos[0] - self.ball_radius, self.ball_pos[1] - self.ball_radius),
                                 size=(self.ball_radius * 2, self.ball_radius * 2))
+
+        exit_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.1))
+        exit_button = Button(text='Exit Game', on_press=self.exit)
+        exit_layout.add_widget(exit_button)
+        self.add_widget(exit_layout)
+
+    def exit(self, instance):
+        app = App.get_running_app()
+        app.root.current = 'StartScreen'
 
     def update(self, dt):
         self.move_ball()
@@ -65,12 +60,11 @@ class PinballGame(Widget):
         self.ball.pos = (self.ball_pos[0] - self.ball_radius, self.ball_pos[1] - self.ball_radius)
 
     def check_collision(self):
-        # Add collision detection logic here
         pass
 
     def on_touch_down(self, touch):
-        # Add any touch events you want to handle here
         pass
+
 
 class PinballApp(App):
     def build(self):
@@ -85,7 +79,7 @@ class PinballApp(App):
         layout = self.root
         layout.remove_widget(self.start_screen)
 
-        game = PinballGame(start_screen_callback=self.show_start_screen, game_over_callback=self.show_game_over)
+        game = PinballGame(start_screen_callback=self.show_start_screen)
         layout.add_widget(game)
 
         Clock.schedule_interval(game.update, 1.0 / 60.0)
@@ -97,14 +91,6 @@ class PinballApp(App):
 
         self.start_screen = StartScreen(start_game_callback=self.start_game)
         layout.add_widget(self.start_screen)
-
-    def show_game_over(self):
-        layout = self.root
-        game = layout.children[-1]
-        layout.remove_widget(game)
-
-        game_over_screen = GameOverScreen(restart_game_callback=self.start_game)
-        layout.add_widget(game_over_screen)
 
 if __name__ == '__main__':
     PinballApp().run()
